96e4ffea799d9caab88598aa0036763e8786ea5c
Cameron <cholbrook582@gmail.com>
2022-01-12 13:20:23 -0800

    Adding a quickstart file.

---
+++ desktop-quickstart.bat
@@ -0,0 +1,7 @@
+@echo off
+
+start C:\"64-Bit Programs"\JetBrains\"PyCharm Community Edition 2021.3"\bin\pycharm64.exe
+
+start chrome https://github.com/CameronH64/daily-weather-retrieval-tool
+
+start C:\"Program Files"\Git\git-bash.exe
