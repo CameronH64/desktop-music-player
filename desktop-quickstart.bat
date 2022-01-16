d40521fe4667742d3c9553a83bf7cc9389d040c2
Cameron <cholbrook582@gmail.com>
2022-01-16 11:32:23 -0800

    Updating quickstart.bat

--- desktop-quickstart.bat
+++ desktop-quickstart.bat
@@ -2,6 +2,6 @@
 
 start C:\"64-Bit Programs"\JetBrains\"PyCharm Community Edition 2021.3"\bin\pycharm64.exe
 
-start chrome https://github.com/CameronH64/daily-weather-retrieval-tool
+start chrome https://github.com/CameronH64/desktop-music-player
 
 start C:\"Program Files"\Git\git-bash.exe
